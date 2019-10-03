12f12600f7f3605419386b0cb1e31b03ff73e93a
Bethany Wride <bethanywride@Bethanys-MacBook-Air-2.local>
2019-10-03 17:58:59 -0600

    Created initial Xcode project and Starship model

---
+++ StarWarsStarShips/StarWarsStarShips/Starship.swift
@@ -0,0 +1,31 @@
+//
+//  Starship.swift
+//  StarWarsStarShips
+//
+//  Created by Bethany Wride on 10/3/19.
+//  Copyright © 2019 Bethany Wride. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResults: Decodable {
+    let starships: [Starship]
+}
+
+struct Starship: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case model
+        case cost = "cost_in_credits"
+        case speed = "max_atmosphering_speed"
+        case films
+    }
+    
+    let name: String
+    let model: String
+    let cost: String
+    let speed: String
+    let films: [String]
+}
+
---
+++ StarWarsStarShips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsStarShips
+//
+//  Created by Bethany Wride on 10/3/19.
+//  Copyright © 2019 Bethany Wride. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
